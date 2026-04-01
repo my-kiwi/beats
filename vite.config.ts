f004344feed387e527321cff73ebdaa51ccc81e0
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-04-01 14:49:49 +0200

    feat: add pwa basic options

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,29 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   base: './', // Ensures relative paths are used in the generated HTML,
    plugins: [
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Beats',
+        short_name: 'Beats',
+        description: 'Beats app',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥁</text></svg>',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥁</text></svg>',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,ts,tsx,css,html,ico,png,svg,mp3}'],
+      },
+    }),
   ]
 });
